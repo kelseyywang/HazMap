4f8ae41ba413dddc37a8ca547dbfa47b943f938e
Ken Chen <lbkchen@gmail.com>
2017-10-22 12:52:32 -0700

    Fixes routes.

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :checkins do
-    get 'clusters', to: 'checkins#clusters'
+    collection do
+      get 'clusters', to: 'checkins#clusters'
+    end
   end
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
