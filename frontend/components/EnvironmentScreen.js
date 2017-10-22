17a4eaa52103af2e0bbf62477c7d99c3eae36441
Vanessa Ng <vanesng96@gmail.com>
2017-10-22 12:02:12 -0700

    back and submit button for Environment Screen

--- frontend/components/EnvironmentScreen.js
+++ frontend/components/EnvironmentScreen.js
@@ -5,6 +5,7 @@ import PushDown from '../common/PushDown';
 import Placeholder from '../common/Placeholder';
 import Button from '../common/Button';
 import commonStyles from '../styles/commonStyles';
+import colors from '../styles/colors';
 
 export default class EnvironmentScreen extends React.Component {
   static navigationOptions = {
@@ -21,74 +22,123 @@ export default class EnvironmentScreen extends React.Component {
     this.setState({})
   }
 
-  render() {
+  render() {   
+    const { navigate } = this.props.navigation;
     return (
-      <ScrollView style={{backgroundColor: '#EFA649'}}>
-      <Text>Anything unusual in your area?</Text>
-       <View style={commonStyles.viewStyle}>
-        <Text>Air</Text>
-
-        <Button
-          onPress={this.handleClick}
-          title='Smoke'
-          main
-        />
-
-        <Button
-          onPress={this.handleClick}
-          title='Haze'
-          main
-        />
-
-       <Text>Water</Text>
+      <ScrollView style={{backgroundColor: colors.backgroundColor}}>
+      <View contentContainerStyle={commonStyles.viewStyle}>
+        <PushDown />
+        <Placeholder>
+          <Text style={commonStyles.bigTextStyle}>Anything unusual in your area?</Text>
+        </Placeholder>
+         <View style={{flex: 4}}>
+          <Text style={commonStyles.titleTextStyle}>Air</Text>
 
           <Button
-            onPress={this.handleClick}
+            onPress={this.handleClick1}
+            title='Smoke'
+            marginSide={25}
+            margin={8}
+            main
+          />
+
+          <Button
+            onPress={this.handleClick2}
+            title='Haze'
+            marginSide={25}
+            margin={8}
+            main
+          />
+
+        <Text style={commonStyles.titleTextStyle}>Water</Text>
+
+          <Button
+            onPress={this.handleClick3}
+            title='Stagnant Water'
+            marginSide={25}
+            margin={8}
+            main
+          />
+
+          <Button
+            onPress={this.handleClick4}
+            title='Discolored Water'
+            marginSide={25}
+            margin={8}
+            main
+          />
+
+          <Button
+            onPress={this.handleClick4}
             title='Abnormal smell or taste'
+            marginSide={25}
+            margin={8}
             main
           />
+
+          <Text style={commonStyles.titleTextStyle}>Contaminant Exposure</Text>
+
+            <Button
+              onPress={this.handleClick5}
+              title='Mold, Bacteria Growth'
+              marginSide={25}
+              margin={8}
+              main
+            />
+
+            <Button
+              onPress={this.handleClick6}
+              title='Chemical Spills'
+              marginSide={25}
+              margin={8}
+              main
+            />
+            <Text style={commonStyles.titleTextStyle}>Pests</Text>
+
+            <Button
+              onPress={this.handleClick7}
+              title='Rodents'
+              marginSide={25}
+              margin={8}
+              main
+            />
+
+            <Button
+              onPress={this.handleClick8}
+              title='Insects'
+              marginSide={25}
+              margin={8}
+              main
+            />
+
+          <Text style={commonStyles.titleTextStyle}>Other</Text>
+            <Button
+              onPress={this.handleClick9}
+              title='Other'
+              marginSide={25}
+              margin={8}
+              main
+            />
 
           <Button
-            onPress={this.handleClick}
-            title='Discolored water'
-            main
-          />
+              onPress={() => navigate('MapScreen', { username: this.props.navigation.state.params.username })}
+              title='SUBMIT'
+              marginSide={25}
+              margin={8}
+              main
+            />
 
           <Button
-            onPress={this.handleClick}
-            title='Stagnant water'
-            main
-          />
+              onPress={() => navigate('MapScreen', { username: this.props.navigation.state.params.username })}
+              title='BACK'
+              marginSide={25}
+              margin={8}
+              main
+            />
 
-        <Text>Contaminant Exposure</Text>
-
-          <Button
-            onPress={this.handleClick}
-            title='Mold, Bacteria Growth'
-            main
-          />
-
-          <Button
-            onPress={this.handleClick}
-            title='Chemical Spills'
-            main
-          />
-
-        <Text>Pests</Text>
-
-          <Button
-            onPress={this.handleClick}
-            title='Rodents'
-            main
-          />
-
-          <Button
-            onPress={this.handleClick}
-            title='Insects'
-            main
-          />
-
-      </View>
+            <PushDown />
+          </View>
+        </View>
       </ScrollView>
     );
   }
