f2e30515f4f6e4c622aed2b0ace418e7ff7bf798
Vanessa Ng <vanesng96@gmail.com>
2017-10-22 12:14:06 -0700

    selected button color

--- frontend/styles/colors.js
+++ frontend/styles/colors.js
@@ -2,8 +2,8 @@ const colors = {
   mainTextColor: '#fff',
   mainButtonColor: 'rgba(0, 0, 0, 0)',
   mainButtonTextColor: '#fff',
-  secondaryButtonColor: 'rgba(0, 0, 0, 0)',
-  secondaryButtonTextColor: 'rgba(0, 0, 0, .5)',
+  secondaryButtonColor: '#fff',
+  secondaryButtonTextColor: '#F28647',
   backgroundColor: '#F28647',
   circleColor: 'rgba(190, 0, 0, .3)',
 };
